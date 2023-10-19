4a2f782bc811f2b758344de4b9d99f37d8aaf1f3
johnyojohn <68317292+johnyojohn@users.noreply.github.com>
2023-10-19 19:32:49 -0400

    Setup home and other style changes
    changed favicon too

--- personal_website/src/components/Layout.js
+++ personal_website/src/components/Layout.js
@@ -1,28 +1,86 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
 const Layout = ({ children, title = "My Personal Website" }) => {
-  return <>
-    <Head>
-      <title>{title}</title>
-    </Head>
-    <nav className="bg-gray-800 text-white p-4">
-      <div className="container mx-auto">
-        <Link href="/" className="px-2">
-          Home
-        </Link>
-        <Link href="/about" className="px-2">
-          About Me
-        </Link>
-        <Link href="/blog" className="px-2">
-          Blog
-        </Link>
+  const router = useRouter();
+
+  const isActive = (route) => {
+    if (route === "/blog") {
+      return router.pathname === route || router.pathname.startsWith("/blog/");
+    }
+    return router.pathname === route;
+  };
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <div className="antialiased max-w-3xl mx-4 mt-8 lg:mx-auto">
+        <nav className="bg-white">
+          <div className="flex justify-between items-center p-4">
+            <div className="text-lg font-semibold text-gray-900">
+              <Link href="/">
+                <Image
+                  src="/logo-dalle.png"
+                  alt="My Website Logo"
+                  width={50}
+                  height={50}
+                />
+              </Link>
+            </div>
+            <div className="space-x-4 relative">
+              <Link href="/">
+                <span
+                  className={`cursor-pointer text-gray-800 hover:text-gray-600 ${
+                    isActive("/") ? "active-link" : ""
+                  }`}
+                >
+                  Home
+                </span>
+              </Link>
+              <Link href="/about">
+                <span
+                  className={`cursor-pointer text-gray-800 hover:text-gray-600 ${
+                    isActive("/about") ? "active-link" : ""
+                  }`}
+                >
+                  About Me
+                </span>
+              </Link>
+              <Link href="/blog">
+                <span
+                  className={`cursor-pointer text-gray-800 hover:text-gray-600 ${
+                    isActive("/blog") ? "active-link" : ""
+                  }`}
+                >
+                  Blog
+                </span>
+              </Link>
+              <span className="absolute h-0.5 bg-blue-500 transition-all duration-300 ease-in-out underline"></span>
+            </div>
+          </div>
+        </nav>
+        <main className="p-4">{children}</main>
       </div>
-    </nav>
-    <div className="container mx-auto p-4">
-      <main>{children}</main>
-    </div>
-  </>;
+      <style jsx>{`
+        .active-link {
+          border-bottom: 2px solid #0a93a8;
+          padding-bottom: 3px;
+        }
+        .underline {
+          width: 0;
+          bottom: 0;
+          left: 0;
+        }
+        .active-link ~ .underline {
+          width: 100%;
+        }
+      `}</style>
+    </>
+  );
 };
 
 export default Layout;
--- personal_website/src/pages/index.js
+++ personal_website/src/pages/index.js
@@ -4,9 +4,30 @@ export default function Home() {
   return (
     <Layout title="Home">
       <div className="text-center">
-        <h1 className="text-5xl mb-5">Hello, I'm John Seungjin Oh</h1>
-        <p className="text-lg">This is the home page.</p>
+        <h1 className="text-5xl mb-5 mt-8">Hello, I'm John Seungjin Oh</h1>
+        <p className="text-lg text-[#e43a46]">
+          This is my personal website and blog.
+        </p>
       </div>
+      <p className="prose prose-neutral max-w-none mt-20">
+        Go to the "About Me" page to view my socials/contacts, projects, resume,
+        and other miscellaneous information about me.
+        <br />
+        <br />
+        Go to the "Blog" page to view my blog. I write regularly (hopefully
+        around once a week) about philosophy, computer science, math, books, and
+        sometimes even my own opinions. Although the blog is pretty empty for
+        now, I'm in the process of migrating some of my past writing. It will
+        also get new content with some time.
+        <br />
+        <br />
+        This website was created pretty much from scratch (a pedantic would say
+        that no developer creates anything from scratch since everyone, and
+        especially developers, and <i>especially</i> web developers, all stand
+        on the shoulders of giants; I would not disagree) while I was learning
+        Next.js and Tailwind CSS. The website's icon was produced by DALL-E 3
+        during an unsuccesful attempt to emulate Rothko paintings.
+      </p>
     </Layout>
   );
 }
