8c05a69a56c4d1767c74958c97f5f3934fc5896b
johnyojohn <68317292+johnyojohn@users.noreply.github.com>
2023-10-22 18:34:35 -0400

    added Vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,14 @@
 // These styles apply to every route in the application
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
+
+export default MyApp;
