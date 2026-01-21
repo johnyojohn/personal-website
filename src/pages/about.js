6658fa23b4f1c4e5558296cfc1aa05a778cde215
johnyojohn <68317292+johnyojohn@users.noreply.github.com>
2026-01-21 18:49:09 -0500

    Updated projects and desc

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,22 +13,22 @@ export default function About() {
       <div className="">
         <h1 className="text-4xl font-mono mb-8">about me</h1>
         <p className="prose prose-neutral max-w-none">
-          My legal name is John Seungjin Oh. I also go by John Oh, Seungjin Oh,
-          Johnyojohn, and (with acute awareness that a public pseudoynm is
-          cringe) Anti-Dionysus.
+          I'm John Seungjin Oh.
           <br />
-          I am a second-year undergraduate student at Carnegie Mellon's School
-          of Computer Science hoping to graduate in 2026. I am also pursuing an
+          I am a third-year undergraduate student at Carnegie Mellon's School
+          of Computer Science, graduating in May 2027. I am also pursuing an
           additional major in philosophy, with a particular interest in logic.
           Occasionally, I notice my heart whispering into my ear that I should
           take on a major in math as well, tempting me with nLab and cool
           topological visualizations and the intricate connections math has with
           philosophy. But I can only count to two.
           <br />
-          My biggest focuses within computer science right now are large
-          language models (LLMs), algorithms, and web development. Within
-          philosophy, my biggest interests are causality, the "later" Nietzsche,
-          philosophy of A.I., and social theory (Debord, Baudrillard, Han).
+          My biggest focuses within computer science right now are ML, education technology, and AR. Within
+          philosophy, my biggest interests are epistemology, causality, the "later" Nietzsche, Baudrillard, and
+          philosophy of A.I.
+          <br />
+          I believe that every single person in the world is entitled to a life worth living.
+          So, I want to give everyone what they deserve. This is my justice.
         </p>
 
         {/* Socials/Contacts */}
@@ -86,6 +86,35 @@ export default function About() {
         <section className="my-8 mt-8">
           <h2 className="text-2xl mb-4 text-customRed font-mono">projects</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="p-4 border rounded">
+              <h3 className="text-xl mb-1 text-customBlue">
+                3D Pen in VR
+              </h3>
+              <hr className="mb-3" width="100%" color="black" />
+              <p>
+                Group project from CMU's Extended Reality course. Developed for Meta Quest 2/3 using Unity.
+                We built a 3D "pen" that you can use in VR to draw 3D lines and manifolds.
+                We also implemented a coordinate system that the user can extend, translate,
+                rotate, scale, and place 3D objects in. 
+                <br />
+                I was inspired by how when we solve geometry problems, we often use graph paper to
+                draw out the problem and solve it. When faced with a 3D problem, we have no choice but to 
+                settle with 2D projections, since we're limited by 2D pen and paper. With VR, this limitation
+                is gone! Many ordinary 3D shapes can be drawn using only a 3D pen and a boundary-filling algorithm.
+              </p>
+              <a
+                href="https://github.com/syh379/IXR-final-project"
+                className="text-blue-500"
+              >
+                Github Repo
+              </a>
+              <a
+                href="/videos/FinalProjectWalkthrough.mp4"
+                className="text-blue-500 ml-2"
+              >
+                Walkthrough 
+              </a>
+            </div>
             <div className="p-4 border rounded">
               <h3 className="text-xl mb-1 text-customBlue">
                 TextualLearner: Convert Educational Videos Into Articles (in
